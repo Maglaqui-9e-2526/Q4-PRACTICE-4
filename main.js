ae5fbccc7e0bb4b1c7c8b8ad6c2666147fd608c4
Maglaqui-9e-2526 <maglaqui.fs@obmontessori.edu.ph>
2026-04-17 10:39:32 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function getopen() {
+
+    let Carcounter = 6;
+    const maxCars = 40;
+
+    while (Carcounter < maxCars) {
+    window.alert("Car number" + Carcounter + " Is Entering")
+    Carcounter += 1;
+    } window.alert("Garage is Full")
+} 
